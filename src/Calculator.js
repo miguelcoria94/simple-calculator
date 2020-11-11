a7103f8f226eb09ff38da44d6ee16427c9c160cf
Jhonathan Ade <ade.jhonathan24@gmail.com>
2020-11-11 16:34:44 -0500

    done with phase 4

--- src/Calculator.js
+++ src/Calculator.js
@@ -10,10 +10,23 @@ class Calculator extends React.Component {
         }
     }
 
+    handleFirstNum = e => {
+        // console.log(e);
+        const num1 = parseInt(e.target.value, 10);
+        this.setState({num1})
+    }
+    handleSecondNum = e => {
+        // console.log(e);
+        const num2 = parseInt(e.target.value, 10);
+        this.setState({ num2 });
+    }
+
     render() {
         return (
             <div>
-                <h1>Time for math!</h1>
+                <h1>Result:{this.state.result}</h1>
+                <input onChange={this.handleFirstNum} type='number' placeholder='first number' value={this.state.num1} />
+                <input onChange={this.handleSecondNum} type='number' placeholder='second number' value={this.state.num2} />
             </div>
         )
     }
